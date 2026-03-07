81aff9499e7c8936dcaddbc1d960266f5d19b1bd
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-03-07 14:00:05 +0800

    feat : initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="font-bold text-center min-h-screen flex justify-center items-center">
+        hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
