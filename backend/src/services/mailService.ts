ee1a7afb6d09e6437cbdf0cacee5d30625e25e49
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-01 17:59:07 +0800

    fix: remove unnecessary TLS configuration from nodemailer transport in mailService.ts

--- backend/src/services/mailService.ts
+++ backend/src/services/mailService.ts
@@ -1,5 +1,7 @@
 import nodemailer from "nodemailer"
 import dotenv from "dotenv"
+import net from "net"
+
 
 dotenv.config();
 
@@ -13,13 +15,10 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    tls: {
-        rejectUnauthorized: false,
-    },
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASSWORD,
-    }
+    },
 } as nodemailer.TransportOptions)
 
 export async function sendEmailService(payload: emailPayload) : Promise<void> {
