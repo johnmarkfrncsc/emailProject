1248f74c245ed2afa2ed13ce1ec13f025714e59e
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-01 16:17:22 +0800

    fix: add missing slash and use VITE_ env variable in emailService.ts

--- frontend/src/services/emailService.ts
+++ frontend/src/services/emailService.ts
@@ -11,7 +11,7 @@ export async function sendEmail(payload:emailPayload): Promise <void> {
     
     try {
         const baseURL = process.env.VITE_APP_API_URL
-         await axios.post(`${baseURL}api/send-email`, payload)
+         await axios.post(`${baseURL}/api/send-email`, payload)
     } catch (error) {
         console.log("Error in emailService.tsx", error)
     }
